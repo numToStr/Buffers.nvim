368c64080831b688e15de426240a89f0702b5c3f
numToStr <vikasraj1911@gmail.com>
2021-11-05 18:01:50 +0530

    rename nuke() to clear()

--- lua/Buffers/api.lua
+++ lua/Buffers/api.lua
@@ -5,7 +5,7 @@ local A = vim.api
 ---@field non_modifiable boolean: Whether to delete non-modifiable buffers
 
 local function notify(deleted, modified)
-	vim.notify("Buffers :: " .. deleted .. " deleted buffer(s), " .. modified .. " modified buffer(s)")
+	vim.notify("Buffers: " .. deleted .. " deleted buffer(s), " .. modified .. " modified buffer(s)")
 end
 
 ---Remove all buffers except the current one
@@ -18,15 +18,15 @@ function M.only(opts)
 
 	local deleted, modified = 0, 0
 
-	for _, n in ipairs(A.nvim_list_bufs()) do
+	for _, buf in ipairs(A.nvim_list_bufs()) do
 		-- If the iter buffer is modified one, then don't do anything
-		if option(n, "modified") then
+		if option(buf, "modified") then
 			-- iter is not equal to current buffer
 			-- iter is modifiable or del_non_modifiable == true
 			-- `modifiable` check is needed as it will prevent closing file tree ie. NERD_tree
 			modified = modified + 1
-		elseif n ~= cur and (option(n, "modifiable") or opts.non_modifiable) then
-			A.nvim_buf_delete(n, {})
+		elseif buf ~= cur and (option(buf, "modifiable") or opts.non_modifiable) then
+			A.nvim_buf_delete(buf, {})
 			deleted = deleted + 1
 		end
 	end
@@ -36,21 +36,21 @@ end
 
 ---Remove all the buffers
 ---@param opts Opts
-function M.nuke(opts)
+function M.clear(opts)
 	opts = opts or {}
 	local option = A.nvim_buf_get_option
 
 	local deleted, modified = 0, 0
 
-	for _, n in ipairs(A.nvim_list_bufs()) do
+	for _, buf in ipairs(A.nvim_list_bufs()) do
 		-- If the iter buffer is modified one, then don't do anything
-		if option(n, "modified") then
+		if option(buf, "modified") then
 			-- iter is not equal to current buffer
 			-- iter is modifiable or del_non_modifiable == true
 			-- `modifiable` check is needed as it will prevent closing file tree ie. NERD_tree
 			modified = modified + 1
-		elseif option(n, "modifiable") or opts.non_modifiable then
-			A.nvim_buf_delete(n, {})
+		elseif option(buf, "modifiable") or opts.non_modifiable then
+			A.nvim_buf_delete(buf, {})
 			deleted = deleted + 1
 		end
 	end
